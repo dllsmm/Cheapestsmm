ab0b1992febd0579ef4edd2346d041a3033bc860
dllsmm <itzaadil74@gmail.com>
2025-07-20 13:57:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  upi_id: "BHARATPE.8F0E0L7R6Y68168@fbpe",
+  upi_id: "umeshyadav4585@fam",
   support_username: "@dllsmm",
-  qr_img: "https://i.ibb.co/6RjNcdJh/Screenshot-20250719-153539.jpg"
+  qr_img: "https://i.ibb.co/C5Nb3KHN/Picsart-25-07-20-13-56-17-268.jpg"
 };
